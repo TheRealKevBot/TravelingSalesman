f0c043119b78dd0d1e8cf09548eff8884d5184b1
TheRealKevBot <Kglas001@gmail.com>
2021-07-30 20:30:57 -0600

    First

---
+++ index.js
@@ -0,0 +1,12 @@
+function paths(n) {
+	
+} 
+
+console.log(paths(1), 1)
+console.log(paths(2), 2)
+console.log(paths(3), 6)
+console.log(paths(4), 24)
+console.log(paths(5), 120)
+console.log(paths(6), 720)
+console.log(paths(7), 5040)
+console.log(paths(8), 40320)
